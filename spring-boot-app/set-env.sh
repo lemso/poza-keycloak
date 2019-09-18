366c803dd39402801ac6f5bd5d64990059f1cf57
Lamine <emailtolamine@gmail.com>
2019-09-19 01:54:14 +0200

    add spring-boot sceanrio

---
+++ spring-boot-app/set-env.sh
@@ -0,0 +1,10 @@
+
+
+curl https://downloads.jboss.org/keycloak/4.8.3.Final/keycloak-4.8.3.Final.zip --output keycloak-4.8.3.Final.zip
+yum install -y unzip
+yum install -y jq
+unzip keycloak-4.8.3.Final.zip
+cd keycloak-4.8.3.Final/bin
+./add-user-keycloak.sh -r master -u admin -p admin
+./standalone.sh -b 0.0.0.0 -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/root/katacoda.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
+
