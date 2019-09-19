c3c4eae6425fc6252a5e90a964ae69ec78bea83e
Lamine <emailtolamine@gmail.com>
2019-09-19 02:33:30 +0200

    customize to poza

--- spring-boot-app/set-env.sh
+++ spring-boot-app/set-env.sh
@@ -8,5 +8,5 @@ apt-get install --assume-yes unzip jq
 unzip keycloak-4.8.3.Final.zip
 cd keycloak-4.8.3.Final/bin
 ./add-user-keycloak.sh -r master -u admin -p admin
-./standalone.sh -b 0.0.0.0 -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/root/katacoda.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
+./standalone.sh -b 0.0.0.0 -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/home/scrapbook/tutorial/katacoda.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
 
