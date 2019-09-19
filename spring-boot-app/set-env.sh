2e6454b54094621b5cfea0e3276d0d77331bdf15
Lamine <emailtolamine@gmail.com>
2019-09-19 03:17:29 +0200

    clone java repo

--- spring-boot-app/set-env.sh
+++ spring-boot-app/set-env.sh
@@ -1,12 +1,19 @@
+apt-get update && apt-get install --assume-yes unzip jq
 
-cd /root/application
+mkdir /home/scrapbook/tutorial/application && cd /home/scrapbook/tutorial/application
 
+# Clone java repo
+git clone https://github.com/lemso/spring-boot-keycloak.git
+
+# Download Keycloak
 curl https://downloads.jboss.org/keycloak/4.8.3.Final/keycloak-4.8.3.Final.zip --output keycloak-4.8.3.Final.zip
-apt-get update
-apt-get install --assume-yes unzip jq
 
+# install keycloak
 unzip keycloak-4.8.3.Final.zip
+
 cd keycloak-4.8.3.Final/bin
 ./add-user-keycloak.sh -r master -u admin -p admin
+
+# Run keycloak and import realm
 ./standalone.sh -b 0.0.0.0 -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/home/scrapbook/tutorial/katacoda.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
 
